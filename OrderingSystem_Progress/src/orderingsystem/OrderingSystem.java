0e9452caacefd5c55f09a36c8dcdbdc6bdbe2fd1
JeusAlbert <57405090+JeusAlbert@users.noreply.github.com>
2019-11-06 09:20:51 +0800

    Add files via upload

---
+++ OrderingSystem_Progress/src/orderingsystem/OrderingSystem.java
@@ -0,0 +1,12 @@
+package orderingsystem;
+
+public class OrderingSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
